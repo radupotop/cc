3e32545e5c3987aa031ececf4c90562ffecc5226
Radu Potop <radu@wooptoo.com>
2016-06-06 15:28:14 +0300

    Declare model explicitly

--- schema.py
+++ schema.py
@@ -1,14 +1,17 @@
 from marshmallow import Schema, fields
 # from marshmallow_sqlalchemy import ModelSchema
 from flask_marshmallow import Marshmallow
+from model import *
 
 ma = Marshmallow()
 
 class BankSchema(ma.Schema):
     id = fields.Str()
     class Meta:
+        model = Banks
         fields = ('id', 'bank_name', 'bank_country')
 
 class CardsSchema(ma.Schema):
     class Meta:
+        model = Cards
         fields = ('id', 'name', 'bank_id', 'currency')
