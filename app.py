52987e11a43a6e96e057f751d6b8257f8f8de57d
Radu Potop <radu@wooptoo.com>
2016-06-20 00:56:17 +0300

    Populate with BRD cc details

--- app.py
+++ app.py
@@ -27,5 +27,5 @@ def getCards():
 
 
 if __name__ == '__main__':
-  app.debug = True
-  app.run('0.0.0.0', 8080)
+    app.debug = True
+    app.run('0.0.0.0', 8080)
--- populate.py
+++ populate.py
@@ -20,3 +20,14 @@ with app.app_context():
     db.session.add(amex)
 
     db.session.commit()
+
+
+    brd_cum_vrei_tu = Cards(
+        card_name='BRD Cum vrei tu',
+        currency='RON',
+        interest_rate=14.72,
+        interst_free_period='60 days',
+        minimum_repayment='2%',
+        max_credit_limit=22000,
+        eligibility='Must be employed for 3 months minimum.'
+    )
