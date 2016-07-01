0d61daff85f69193f70198ffd80e0bf9a9979d0a
Radu Potop <radu@wooptoo.com>
2016-07-01 18:19:42 +0300

    Additional properties added to model, bt star card

--- model.py
+++ model.py
@@ -27,6 +27,7 @@ class Cards(db.Model):
     bank_id = db.Column(db.Integer, db.ForeignKey('banks.id'), nullable=False)
 
     card_name = db.Column(db.String(255), nullable=False)
+    type = db.Column(db.Enum('VISA', 'Mastercard', 'Amex', 'JCB'))
     currency = db.Column(db.String(3))
 
     # Usually there's a basic annual interest rate,
@@ -40,8 +41,10 @@ class Cards(db.Model):
     cash_withdraw_fee = db.Column(db.String(40))
     interest_free_period = db.Column(db.Integer) # in days if ballance is paid in full
     
+    opening_fee = db.Column(db.Float)
     monthly_fee = db.Column(db.Float)
     yearly_fee = db.Column(db.Float)
+    other_fees = db.Column(db.String(255))
 
     minimum_repayment = db.Column(db.String(60))
 
@@ -51,6 +54,11 @@ class Cards(db.Model):
 
     min_credit_limit = db.Column(db.Integer)
     max_credit_limit = db.Column(db.Integer)
+    credit_limit_comment = db.Column(db.String(255))
+
+    # Renews every X years
+    renew_years = db.Column(db.Integer)
+    allows_additional_cards = db.Column(db.Boolean)
 
     # Most cards offer some sort of promotion, 
     # like the first three months interest free, or more cashback.
--- populate.py
+++ populate.py
@@ -24,6 +24,7 @@ with app.app_context():
 
     brd_cum_vrei_tu = Cards(
         card_name='BRD Cum vrei tu',
+        type='Mastercard',
         currency='RON',
         interest_rate=14.72,
         interst_free_period='60 days',
@@ -34,4 +35,25 @@ with app.app_context():
         internet_banking='MyBRD Net, MyBRD Mobile',
         is_contactless=True,
         offer_url='https://www.brd.ro/persoane-fizice/carduri/carduri-de-credit/cardul-de-credit-cumvreitu',
+        renew_years=3,
+    )
+
+    bt_star_forte = Cards(
+        card_name='BT Star Forte',
+        type='Mastercard',
+        currency='RON',
+        interest_rate=24.00,
+        interst_free_period='56 days',
+        credit_limit_comment='Pana la de 5 ori salariul, maximum echivalentul a 5000 EUR in RON',
+        minimum_repayment='10%, intre 1 si 25 a lunii',
+        opening_fee = 0,
+        yearly_fee = 25,
+        eligibility='1 an angajare',
+        offer_url='http://www.starbt.ro/',
+        promotion='12 rate fara dobanda, 8500 magazine partenere, puncte Star, 1 punct = 1 leu',
+        renew_years=5,
+        is_contactless=True,
+        allows_additional_cards=True,
+        other_fees='http://www.starbt.ro/files/star-forte/comisioane_star_forte.pdf',
+
     )
